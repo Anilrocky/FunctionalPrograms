b6c64cc663790a916553912acaa4a990289d62e8
Anilrocky <anilkumarac96@gmail.com>
2023-03-07 15:34:41 +0530

    [Anil] Add:Flip coin and print percentage of Heads and Tails

--- FunctionalPrograms/Program.cs
+++ FunctionalPrograms/Program.cs
@@ -6,6 +6,24 @@ namespace FunctionalPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Functional Problem Statements");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Choose an Option to Perform\n1.Flip Coin\n2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FlipCoin flipCoin = new FlipCoin();
+                        Console.WriteLine("Enter number of Flips");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        flipCoin.Flip(num);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }    
         }
     }
 }
