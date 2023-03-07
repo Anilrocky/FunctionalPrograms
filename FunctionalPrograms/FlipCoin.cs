624c0ff3f17bb8d67071a49bcd94867a98e89b18
Anilrocky <anilkumarac96@gmail.com>
2023-03-07 15:38:10 +0530

    [Anil] Add:Flip coin and print percentage of Heads and Tails

---
+++ FunctionalPrograms/FlipCoin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace FunctionalPrograms
+{
+    public class FlipCoin
+    {
+        const double VALIDATION_CHECK = 0.5;
+        public void Flip(int numOfTimes)
+        {
+            int head = 0, tail = 0;
+            Random random = new Random();
+            for (int i = 0; i < numOfTimes; i++)
+            {
+                double value = random.NextDouble();
+                if (value < VALIDATION_CHECK)
+                    tail++;                    
+                else
+                    head++;                  
+            }
+            double headPercentage = (head*100/numOfTimes);
+            double tailPercentage = (tail*100/numOfTimes);
+            Console.WriteLine("Head Percentage--->  " + headPercentage + "\nTail Percentage--->  " + tailPercentage);
+        }
+    }
+}
