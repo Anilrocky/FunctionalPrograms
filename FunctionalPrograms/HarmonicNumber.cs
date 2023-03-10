279251dda4f479a2791e929e60199f2458d21ea1
Anilrocky <anilkumarac96@gmail.com>
2023-03-10 07:16:20 +0530

    [Anil] Refactor:Harmonic form

--- FunctionalPrograms/HarmonicNumber.cs
+++ FunctionalPrograms/HarmonicNumber.cs
@@ -8,8 +8,10 @@ namespace FunctionalPrograms
             double result = 0;
             for (double i = 1; i <=number; i++)
             {              
-                Console.Write("1/"+i+"+",result);
+                Console.Write("1/"+i);
                 result += 1 / i;
+                if (i<number)
+                    Console.Write("+");
             }            
             Console.WriteLine("\nHarmonic Value of " + number + " is: " + result);
         }
